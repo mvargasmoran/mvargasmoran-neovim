e926e4af35056156acff2bb71846d691954e7514
mvargasmoran <mvargasmoran@gmail.com>
2024-04-18 22:10:21 -0400

    Explain myself about the personal plugin stuff
    leaving whatever kickstart is doing alone is great

--- init.lua
+++ init.lua
@@ -227,7 +227,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  { import = 'mplugins' },
+  { import = 'mplugins' }, -- personal plugins  (Not Kickstart stuff)
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
