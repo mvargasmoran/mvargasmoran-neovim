7c7fe7f256ca95dc9b691b3d093d98837848f8eb
mvargasmoran <mvargasmoran@gmail.com>
2024-04-27 03:05:08 -0400

    Replace tabs with 2 spaces
    Maybe I will move back to 4 spaces

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.o.statuscolumn = '%s %l %r '
+
+-- some niceties
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
