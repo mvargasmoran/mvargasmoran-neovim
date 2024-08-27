0f1b024f0082e42a5312e0ced9a848af80455800
mvargasmoran <mvargasmoran@gmail.com>
2024-08-27 02:05:08 -0400

    Add a piece of documentation for harpoon keymaps
    I won't ever remember the commands.

--- lua/mplugins.lua
+++ lua/mplugins.lua
@@ -8,6 +8,7 @@ return {
   'EdenEast/nightfox.nvim',
   'haishanh/night-owl.vim',
   'tiagovla/tokyodark.nvim',
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- HARPOON MAN!
   {
@@ -29,3 +30,22 @@ return {
     end
   }
 }
+
+  -- keymaps = {
+  --   ["g?"] = "actions.show_help",
+  --   ["<CR>"] = "actions.select",
+  --   ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+  --   ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+  --   ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+  --   ["<C-p>"] = "actions.preview",
+  --   ["<C-c>"] = "actions.close",
+  --   ["<C-l>"] = "actions.refresh",
+  --   ["-"] = "actions.parent",
+  --   ["_"] = "actions.open_cwd",
+  --   ["`"] = "actions.cd",
+  --   ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+  --   ["gs"] = "actions.change_sort",
+  --   ["gx"] = "actions.open_external",
+  --   ["g."] = "actions.toggle_hidden",
+  --   ["g\\"] = "actions.toggle_trash",
+  -- },
